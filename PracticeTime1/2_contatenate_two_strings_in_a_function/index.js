e5c5d3180cbd835c0b3973bee4d965fee7da441f
JLMaynardDesign <JLMaynardDesign@gmail.com>
2022-02-13 21:53:14 -0500

    completed practice 2

---
+++ PracticeTime1/2_contatenate_two_strings_in_a_function/index.js
@@ -0,0 +1,10 @@
+let name = "Linda";
+let greeting = "Hi there";
+
+// Create a function that logs out "Hi there, Linda!" when called
+const personalGreeting = function() {
+  let output = greeting + ", " + name + "!";
+  console.log(output);
+};
+
+personalGreeting();
