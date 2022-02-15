5b9e0340cdd64552c0ebf27bff91f9e6c6bf2574
JLMaynardDesign <JLMaynardDesign@gmail.com>
2022-02-14 23:57:40 -0500

    commit

---
+++ PracticeTime1/5_rendering_an_error_message/index.js
@@ -0,0 +1,13 @@
+// When the user clicks the purchase button, render out
+// "Something went wrong, please try again" in the paragraph
+// that has the id="error".
+
+let errorParagraph = document. getElementById("error");
+//console.log(errorParagraph)
+
+//registering that the user clicks the button
+
+function purchase() {
+  //console.log("button clicked")
+
+}
