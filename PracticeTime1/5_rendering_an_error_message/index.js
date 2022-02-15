f5acc93c93538d4ab4b8d4c96e8d4cca8aaebbf0
JLMaynardDesign <JLMaynardDesign@gmail.com>
2022-02-15 12:36:54 -0500

    completed all practices

--- PracticeTime1/5_rendering_an_error_message/index.js
+++ PracticeTime1/5_rendering_an_error_message/index.js
@@ -2,12 +2,12 @@
 // "Something went wrong, please try again" in the paragraph
 // that has the id="error".
 
-let errorParagraph = document. getElementById("error");
+let errorParagraph = document.getElementById("error");
 //console.log(errorParagraph)
 
 //registering that the user clicks the button
 
 function purchase() {
   //console.log("button clicked")
-
-}
+  errorParagraph.textContent = "Something went wrong, please try again";
+};
